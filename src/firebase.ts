1244c4f584398478c568931b7fc20e66c96fa8fb
Wata-Naoki <nao70blog@gmail.com>
2022-10-28 11:35:56 +0900

    firebase認証エラー

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,7 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
